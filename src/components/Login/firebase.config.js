15cfed3c8fd924e13b801e6ccd89a50a3c364440
Tanviras <tanvirasme@gmail.com>
2021-03-18 10:39:31 +0600

    authentication done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAOSiHgBoYja6o4o2KsiVzyFf9Y4C8Gs40",
+  authDomain: "burj-khalifa-b53d3.firebaseapp.com",
+  projectId: "burj-khalifa-b53d3",
+  storageBucket: "burj-khalifa-b53d3.appspot.com",
+  messagingSenderId: "926959575115",
+  appId: "1:926959575115:web:408ee502262f83aec12dce"
+};
+
+  export default firebaseConfig;

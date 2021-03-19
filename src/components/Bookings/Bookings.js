76c363d02f9523c4c51f34d7bb102fb256a2229e
Tanviras <tanvirasme@gmail.com>
2021-03-19 11:41:51 +0600

    verification done using firebase admin

--- src/components/Bookings/Bookings.js
+++ src/components/Bookings/Bookings.js
@@ -7,12 +7,21 @@ const Bookings = () => {
     useEffect(()=>{//Book.js er moddhe backened a data post korar way dekhaisilam,ekhane normal api use kore data niye asha, so normally useEffect toh lagbei. get jehetu kortesi,alada kore get er kotha bola lage na. Post,delete,patch esob korle bola lage Method diye Book.js er moto 
     //kono component theke fetch korle useEffect lagtese.
         //node mongo crud er moddhe index.html er moddhe use effect diye kori nai,direct fetch diye korechilam
-        fetch('http://localhost:5000/bookings?email='+loggedInUser.email)
+
+        //whatever,we have just got data from our own database,using our own api
+        //alongside,we will now send the jwt token to our database
+        fetch('http://localhost:5000/bookings?email='+loggedInUser.email,{
+            // method:'GET',
+            headers:{
+                    'authorization': `Bearer ${sessionStorage.getItem("token")}`,
+                    'Content-type':'application/json'
+                    }
+        })
         .then(res=>res.json())
         .then(data=>{
             setBookings(data);
         })
-    },[]);
+    });
 
     return (
         <div>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -25,13 +25,31 @@ const Login = () => {
             const {displayName, email} = result.user;
             const signedInUser = {name: displayName, email} 
             setLoggedInUser(signedInUser);
+            storeAuthToken();//to get jwt token,we have made this function
             history.replace(from);
-            // ...
           }).catch(function(error) {
             const errorMessage = error.message;
             console.log(errorMessage);
           });
     }
+    
+    //to get jwt token for loggedInUser
+    //https://firebase.google.com/docs/auth/admin/verify-id-tokens#web
+
+    const storeAuthToken=()=>{
+        firebase.auth().currentUser.getIdToken(true)
+        .then(function(idToken) {
+            // console.log(idToken);//we have just seen the idToken which is in encoded form
+            //https://jwt.io/ decodes the jwt token
+
+            //we normally keep this token to sessionStorage
+            sessionStorage.setItem('token',idToken);//token is the given name where idToken is set
+        })
+        .catch(function(error) {
+            // Handle error
+        });
+    }
+
 
     return (
         <div>
